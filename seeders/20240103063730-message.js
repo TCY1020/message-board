42a96ed381f8f40d536224ee0de736d2e5d85c2f
Jason <a25023423@gmail.com>
2024-01-18 10:55:30 +0800

    100 messages

--- seeders/20240103063730-message.js
+++ seeders/20240103063730-message.js
@@ -5,9 +5,16 @@ const { faker } = require('@faker-js/faker');
 module.exports = {
   async up(queryInterface, Sequelize) {
     const user = await User.findAll({ raw: true });
+    const list = [];
+    await Promise.all(user.map(async person => {
+      for (let i = 0; i < 20; i++) {
+        list.push(person.id);
+      }
+    }));
     await queryInterface.bulkInsert('Messages',
-      Array.from({ length: user.length }, (_, item) => ({
-        user_id: user[item].id,
+      Array.from({ length: list.length }, (_, item) => ({
+        id: item + 1,
+        user_id: list[item],
         comment: faker.lorem.sentence({ min: 5, max: 15 }),
         created_at: new Date(),
         updated_at: new Date(),
